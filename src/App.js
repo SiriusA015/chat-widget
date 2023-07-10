1425d0f4a0cf010f5c7753020d06c211f0a21e04
Alexmbli99 <134547680+Alexmbli99@users.noreply.github.com>
2023-07-10 12:04:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ChatWidget from './ChatWidget';
 
 function App() {
+  const [widgetStyles, setWidgetStyles] = useState(null);
+
+  useEffect(() => {
+    const fetchWidgetStyles = async () => {
+      const url = 'https://parchedrotatingcommand.alexli81.repl.co/get-widget-styles';
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            currentUrl: window.location.href,
+          }),
+        });
+        const widgetStyles = await response.json();
+        setWidgetStyles(widgetStyles);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchWidgetStyles();
+  }, []);
+
   return (
     <div className="App">
       {/* Your other app components */}
-      <ChatWidget />
+      <ChatWidget widgetStyles={widgetStyles} setWidgetStyles={setWidgetStyles} />
     </div>
   );
 }
